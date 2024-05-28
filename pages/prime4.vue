2a30742afa9bcefe57a7295fa4499c185a9d7304
Mukund Parashar <mukund-p001@webreinvent.com>
2024-05-28 17:59:35 +0530

    Added: glow buttons

--- pages/prime4.vue
+++ pages/prime4.vue
@@ -42,7 +42,9 @@
       }">
     <StepperPanel header="Header I">
       <template #header>
-        <div class="flex flex-row align-items-center border-1 border-white hover:border-black-alpha-90 hover:bg-purple-100 cursor-pointer  p-2 border-round-3xl gap-1">
+        <div class="flex flex-row align-items-center  cursor-pointer  p-2 border-round-3xl gap-1"
+             :class="!glowBtn.one? ' border-1 bg-purple-100':''"
+        >
           <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
             <g fill="none" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="m8 12.5l3 3l5-6" />
@@ -57,13 +59,15 @@
           <div class="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">Content I</div>
         </div>
         <div class="flex pt-4 justify-content-end">
-          <Button label="Next" icon="pi pi-arrow-right" iconPos="right" @click="nextCallback" />
+          <Button label="Next" icon="pi pi-arrow-right" iconPos="right" @click="nextCallback(); fObj.btn2()" />
         </div>
       </template>
     </StepperPanel>
     <StepperPanel header="Header II">
       <template #header>
-        <div class="flex flex-row align-items-center border-1 border-white hover:border-black-alpha-90 hover:bg-purple-100 cursor-pointer  p-2 border-round-3xl gap-1">
+        <div class="flex flex-row align-items-center cursor-pointer  p-2 border-round-3xl gap-1"
+             :class="!glowBtn.two? ' border-1 bg-purple-100':''"
+        >
           <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
             <path fill="currentColor" d="M12 14L1 8l11-6l11 6zm0 4L1.575 12.325l2.1-1.15L12 15.725l8.325-4.55l2.1 1.15zm0 4L1.575 16.325l2.1-1.15L12 19.725l8.325-4.55l2.1 1.15zm0-10.275L18.825 8L12 4.275L5.175 8zM12 8" />
           </svg>
@@ -73,7 +77,7 @@
       <template #content="{ prevCallback, nextCallback }" >
         <div class="  flex justify-content-center bg-gray-100">
           <div class="flex flex-column  align-items-center w-5 ">
-        <div class="flex flex-row align-items-center border-white hover:border-black-alpha-90 bg-white cursor-pointer  p-2 border-round-3xl gap-1 w-4 mt-4">
+        <div class="flex flex-row align-items-center bg-white hover:bg-purple-50 cursor-pointer  p-2 border-round-3xl gap-1 w-4 mt-4">
           <svg xmlns="http://www.w3.org/2000/svg" class="text-purple-500" width="1.5em" height="1.5em" viewBox="0 0 24 24">
             <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m15.238 10.81l-.569 1.694a4.325 4.325 0 0 1-2.757 2.76l-1.713.569a.288.288 0 0 0 0 .548l1.713.569a4.318 4.318 0 0 1 2.736 2.738l.568 1.715a.287.287 0 0 0 .548 0l.59-1.694a4.323 4.323 0 0 1 2.735-2.738l1.714-.569a.288.288 0 0 0 0-.548l-1.692-.59a4.318 4.318 0 0 1-2.757-2.76l-.569-1.715a.289.289 0 0 0-.448-.126a.288.288 0 0 0-.099.148m-8.43-4.914l-.413 1.231a3.145 3.145 0 0 1-2.006 2.007l-1.246.414a.21.21 0 0 0 0 .398l1.246.415a3.14 3.14 0 0 1 1.99 1.99l.413 1.248a.21.21 0 0 0 .398 0l.43-1.232a3.145 3.145 0 0 1 1.99-1.99l1.245-.415a.21.21 0 0 0 0-.398l-1.23-.43A3.141 3.141 0 0 1 7.62 7.128l-.414-1.247a.21.21 0 0 0-.398.016m7.849-3.422l-.207.616a1.572 1.572 0 0 1-1.002 1.004l-.623.207a.104.104 0 0 0-.052.16a.104.104 0 0 0 .052.039l.623.207a1.57 1.57 0 0 1 .995.995l.206.624a.105.105 0 0 0 .2 0l.214-.616a1.573 1.573 0 0 1 .995-.995l.623-.207a.105.105 0 0 0 0-.2l-.615-.214a1.571 1.571 0 0 1-1.003-1.004l-.207-.624a.105.105 0 0 0-.199.008" />
           </svg>
@@ -106,7 +110,7 @@
               </svg>
               <p class="text-base my-0">Day 1</p>
               </div>
-                <InputSwitch v-model="checked" />
+                <InputSwitch v-model="checked.one" />
               <svg xmlns="http://www.w3.org/2000/svg" class="cursor-pointer" width="1em" height="1em" viewBox="0 0 16 16">
                 <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
                   <circle cx="8" cy="2.5" r=".75" />
@@ -117,7 +121,7 @@
             </div>
             </div>
             <p class="text-base text-gray-500 "> Subject: <span class="text-gray-900"> Reaching out </span></p>
-              <svg class="absolute" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" style="bottom:19%; right:100% " viewBox="0 0 24 24">
+              <svg class="absolute cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" style="bottom:19%; right:100% " viewBox="0 0 24 24">
                 <g fill="none">
                   <path d="M24 0v24H0V0zM12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036c-.01-.003-.019 0-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z" />
                   <path fill="currentColor" d="M9 4.5a1.5 1.5 0 1 1 0 3a1.5 1.5 0 0 1 0-3m1.5 7.5a1.5 1.5 0 1 0-3 0a1.5 1.5 0 0 0 3 0m0 6a1.5 1.5 0 1 0-3 0a1.5 1.5 0 0 0 3 0m6-6a1.5 1.5 0 1 0-3 0a1.5 1.5 0 0 0 3 0M15 16.5a1.5 1.5 0 1 1 0 3a1.5 1.5 0 0 1 0-3M16.5 6a1.5 1.5 0 1 0-3 0a1.5 1.5 0 0 0 3 0" />
@@ -160,7 +164,7 @@
                         </svg>
                         <p class="text-base my-0">Day 4</p>
                       </div>
-                      <InputSwitch v-model="checked" />
+                      <InputSwitch v-model="checked.two" />
                       <svg xmlns="http://www.w3.org/2000/svg" class="cursor-pointer" width="1em" height="1em" viewBox="0 0 16 16">
                         <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
                           <circle cx="8" cy="2.5" r=".75" />
@@ -173,7 +177,7 @@
                   <div class="flex flex-row align-items-center gap-2">
                   <p class="text-base text-gray-500"> Subject: <span class="text-gray-900"> Reaching out </span></p>
                     <div class=" text-xs border-1 py-1 px-2 border-round-3xl border-gray-200">Threaded Follow-up</div>
-                  <svg class="absolute" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" style="bottom:19%; right:100% " viewBox="0 0 24 24">
+                  <svg class="absolute cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" style="bottom:19%; right:100% " viewBox="0 0 24 24">
                     <g fill="none">
                       <path d="M24 0v24H0V0zM12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036c-.01-.003-.019 0-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z" />
                       <path fill="currentColor" d="M9 4.5a1.5 1.5 0 1 1 0 3a1.5 1.5 0 0 1 0-3m1.5 7.5a1.5 1.5 0 1 0-3 0a1.5 1.5 0 0 0 3 0m0 6a1.5 1.5 0 1 0-3 0a1.5 1.5 0 0 0 3 0m6-6a1.5 1.5 0 1 0-3 0a1.5 1.5 0 0 0 3 0M15 16.5a1.5 1.5 0 1 1 0 3a1.5 1.5 0 0 1 0-3M16.5 6a1.5 1.5 0 1 0-3 0a1.5 1.5 0 0 0 3 0" />
@@ -213,7 +217,7 @@
                         </svg>
                         <p class="text-base my-0">Day 10</p>
                       </div>
-                      <InputSwitch v-model="checked" />
+                      <InputSwitch v-model="checked.three" />
                       <svg xmlns="http://www.w3.org/2000/svg" class="cursor-pointer" width="1em" height="1em" viewBox="0 0 16 16">
                         <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
                           <circle cx="8" cy="2.5" r=".75" />
@@ -223,7 +227,7 @@
                       </svg>
                     </div>
                   </div>
-                  <svg class="absolute" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" style="bottom:50%; right:97% " viewBox="0 0 24 24">
+                  <svg class="absolute cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" style="bottom:50%; right:97% " viewBox="0 0 24 24">
                     <g fill="none">
                       <path d="M24 0v24H0V0zM12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036c-.01-.003-.019 0-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z" />
                       <path fill="currentColor" d="M9 4.5a1.5 1.5 0 1 1 0 3a1.5 1.5 0 0 1 0-3m1.5 7.5a1.5 1.5 0 1 0-3 0a1.5 1.5 0 0 0 3 0m0 6a1.5 1.5 0 1 0-3 0a1.5 1.5 0 0 0 3 0m6-6a1.5 1.5 0 1 0-3 0a1.5 1.5 0 0 0 3 0M15 16.5a1.5 1.5 0 1 1 0 3a1.5 1.5 0 0 1 0-3M16.5 6a1.5 1.5 0 1 0-3 0a1.5 1.5 0 0 0 3 0" />
@@ -265,7 +269,7 @@
                         </svg>
                         <p class="text-base my-0">Day 1</p>
                       </div>
-                      <InputSwitch v-model="checked" />
+                      <InputSwitch v-model="checked.four" />
                       <svg xmlns="http://www.w3.org/2000/svg" class="cursor-pointer" width="1em" height="1em" viewBox="0 0 16 16">
                         <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
                           <circle cx="8" cy="2.5" r=".75" />
@@ -276,7 +280,7 @@
                     </div>
                   </div>
                   <p class="text-base text-gray-500"> Subject: <span class="text-gray-900"> Reaching out </span></p>
-                  <svg class="absolute" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" style="bottom:19%; right:100% " viewBox="0 0 24 24">
+                  <svg class="absolute cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" style="bottom:19%; right:100% " viewBox="0 0 24 24">
                     <g fill="none">
                       <path d="M24 0v24H0V0zM12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036c-.01-.003-.019 0-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z" />
                       <path fill="currentColor" d="M9 4.5a1.5 1.5 0 1 1 0 3a1.5 1.5 0 0 1 0-3m1.5 7.5a1.5 1.5 0 1 0-3 0a1.5 1.5 0 0 0 3 0m0 6a1.5 1.5 0 1 0-3 0a1.5 1.5 0 0 0 3 0m6-6a1.5 1.5 0 1 0-3 0a1.5 1.5 0 0 0 3 0M15 16.5a1.5 1.5 0 1 1 0 3a1.5 1.5 0 0 1 0-3M16.5 6a1.5 1.5 0 1 0-3 0a1.5 1.5 0 0 0 3 0" />
@@ -298,13 +302,13 @@
         </div>
 
         <div class="flex py-3 justify-content-between bg-white fixed px-3 " style="bottom:0%; right:0%; left:0% ">
-          <Button label="Back" severity="secondary" class=" border-1 border-gray-400" icon="pi pi-arrow-left" @click="prevCallback" >
+          <Button label="Back" severity="secondary" class=" border-1 border-gray-400" icon="pi pi-arrow-left" @click="prevCallback(); fObj.btn1()" >
             <i class="pi pi-angle-left" ></i>
             <p class="my-0 mx-5">Previous</p>
           </Button>
           <div class="flex align-items-center gap-2">
             <p class="text-sm font-semibold my-0">Your Campaign will be sent to</p>
-            <div class=" flex align-items-center gap-2 text-xs border-1 py-1 px-2 border-round-3xl border-purple-600 bg-purple-100">
+            <div class=" flex align-items-center gap-2 text-xs border-1 py-1 px-2 border-round-3xl border-purple-600 hover:bg-purple-200 bg-purple-100 cursor-pointer">
               <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 20 20">
                 <g fill="currentColor" fill-rule="evenodd" clip-rule="evenodd">
                   <path d="M5 9a2 2 0 1 0 0-4a2 2 0 0 0 0 4m0 1a3 3 0 1 0 0-6a3 3 0 0 0 0 6" />
@@ -316,7 +320,7 @@
               <p class="my-0 font-semibold">24 Contacts</p></div>
 
           </div>
-          <Button label="Next"  icon="pi pi-angle-right" iconPos="right" class="bg-purple-600 border-none hover:bg-purple-700" @click="nextCallback" >
+          <Button label="Next"  icon="pi pi-angle-right" iconPos="right" class="bg-purple-600 border-none hover:bg-purple-700" @click="nextCallback(); fObj.btn3()" >
 
             <p class="my-0 mx-5">Next</p>
             <i class="pi pi-angle-right" ></i>
@@ -326,7 +330,9 @@
     </StepperPanel>
     <StepperPanel header="Header III">
       <template #header>
-        <div class="flex flex-row align-items-center border-1 border-white hover:border-black-alpha-90 hover:bg-purple-100 cursor-pointer  p-2 border-round-3xl gap-1">
+        <div class="flex flex-row align-items-center   cursor-pointer  p-2 border-round-3xl gap-1"
+             :class="!glowBtn.three? ' border-1 bg-purple-100':''"
+        >
           <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
             <path fill="currentColor" d="M19.9 12.66a1 1 0 0 1 0-1.32l1.28-1.44a1 1 0 0 0 .12-1.17l-2-3.46a1 1 0 0 0-1.07-.48l-1.88.38a1 1 0 0 1-1.15-.66l-.61-1.83a1 1 0 0 0-.95-.68h-4a1 1 0 0 0-1 .68l-.56 1.83a1 1 0 0 1-1.15.66L5 4.79a1 1 0 0 0-1 .48L2 8.73a1 1 0 0 0 .1 1.17l1.27 1.44a1 1 0 0 1 0 1.32L2.1 14.1a1 1 0 0 0-.1 1.17l2 3.46a1 1 0 0 0 1.07.48l1.88-.38a1 1 0 0 1 1.15.66l.61 1.83a1 1 0 0 0 1 .68h4a1 1 0 0 0 .95-.68l.61-1.83a1 1 0 0 1 1.15-.66l1.88.38a1 1 0 0 0 1.07-.48l2-3.46a1 1 0 0 0-.12-1.17ZM18.41 14l.8.9l-1.28 2.22l-1.18-.24a3 3 0 0 0-3.45 2L12.92 20h-2.56L10 18.86a3 3 0 0 0-3.45-2l-1.18.24l-1.3-2.21l.8-.9a3 3 0 0 0 0-4l-.8-.9l1.28-2.2l1.18.24a3 3 0 0 0 3.45-2L10.36 4h2.56l.38 1.14a3 3 0 0 0 3.45 2l1.18-.24l1.28 2.22l-.8.9a3 3 0 0 0 0 3.98m-6.77-6a4 4 0 1 0 4 4a4 4 0 0 0-4-4m0 6a2 2 0 1 1 2-2a2 2 0 0 1-2 2" />
           </svg>
@@ -338,14 +344,16 @@
           <div class="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">Content II</div>
         </div>
         <div class="flex pt-4 justify-content-between">
-          <Button label="Back" severity="secondary" icon="pi pi-arrow-left" @click="prevCallback" />
-          <Button label="Next" icon="pi pi-arrow-right" iconPos="right" @click="nextCallback" />
+          <Button label="Back" severity="secondary" icon="pi pi-arrow-left" @click="prevCallback();  fObj.btn2()" />
+          <Button label="Next" icon="pi pi-arrow-right" iconPos="right" @click="nextCallback();  fObj.btn4()" />
         </div>
       </template>
     </StepperPanel>
     <StepperPanel header="Header IV">
       <template #header>
-        <div class="flex flex-row align-items-center border-1 border-white hover:border-black-alpha-90 hover:bg-purple-100 cursor-pointer  p-2 border-round-3xl gap-1">
+        <div class="flex flex-row align-items-center   cursor-pointer p-2 border-round-3xl gap-1"
+        :class="!glowBtn.four? ' border-1 bg-purple-100':''"
+        >
           <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 256 256">
             <path fill="currentColor" d="M247.31 124.76c-.35-.79-8.82-19.58-27.65-38.41C194.57 61.26 162.88 48 128 48S61.43 61.26 36.34 86.35C17.51 105.18 9 124 8.69 124.76a8 8 0 0 0 0 6.5c.35.79 8.82 19.57 27.65 38.4C61.43 194.74 93.12 208 128 208s66.57-13.26 91.66-38.34c18.83-18.83 27.3-37.61 27.65-38.4a8 8 0 0 0 0-6.5M128 192c-30.78 0-57.67-11.19-79.93-33.25A133.5 133.5 0 0 1 25 128a133.3 133.3 0 0 1 23.07-30.75C70.33 75.19 97.22 64 128 64s57.67 11.19 79.93 33.25A133.5 133.5 0 0 1 231.05 128c-7.21 13.46-38.62 64-103.05 64m0-112a48 48 0 1 0 48 48a48.05 48.05 0 0 0-48-48m0 80a32 32 0 1 1 32-32a32 32 0 0 1-32 32" />
           </svg>
@@ -357,7 +365,7 @@
           <div class="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">Content III</div>
         </div>
         <div class="flex pt-4 justify-content-start">
-          <Button label="Back" severity="secondary" icon="pi pi-arrow-left" @click="prevCallback" />
+          <Button label="Back" severity="secondary" icon="pi pi-arrow-left" @click="prevCallback();  fObj.btn3()" />
         </div>
       </template>
     </StepperPanel>
@@ -375,16 +383,48 @@
 
 <script setup>
 const glowBtn = reactive ({
-  one:true,
+  one:false,
   two:true,
   three:true,
   four:true
 })
 
+const fObj = {
+  btn1: function changeCol() {
+    glowBtn.one = !glowBtn.one
+    glowBtn.two = true
+    glowBtn.three = true
+    glowBtn.four = true
+  },
+  btn2: function changeCol() {
+    glowBtn.two = !glowBtn.two
+    glowBtn.one = true
+    glowBtn.three = true
+    glowBtn.four = true
+  },
+  btn3: function changeCol() {
+    glowBtn.three = !glowBtn.three
+    glowBtn.two = true
+    glowBtn.one = true
+    glowBtn.four = true
+  },
+  btn4: function changeCol() {
+    glowBtn.four = !glowBtn.four
+    glowBtn.two = true
+    glowBtn.three = true
+    glowBtn.one = true
+  },
+}
 
 
 
-const checked = ref(false);
+
+const checked = reactive({
+  one:false,
+  two:false,
+  three:false,
+  four:false,
+});
 
 
 </script>
